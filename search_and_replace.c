b98b1b07954ca4fcf637d8c51d93ddda9c90ad5c
Amandine Demuynck <amdemuyn@c3r13s6.42madrid.com>
2023-01-11 17:02:49 +0100

    new srch&rep

--- search_and_replace.c
+++ search_and_replace.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/10 16:34:04 by amdemuyn          #+#    #+#             */
-/*   Updated: 2023/01/10 17:58:47 by amdemuyn         ###   ########.fr       */
+/*   Created: 2023/01/11 16:13:20 by amdemuyn          #+#    #+#             */
+/*   Updated: 2023/01/11 16:51:21 by amdemuyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,14 @@ int	main(int argc, char **argv)
 	int	i;
 
 	i = 0;
-	if (argc == 4)
+	if (argc == 4 && argv[2][1] == '\0' && argv[3][1] == '\0')
 	{
-		if (argv[2][1] == '\0' && argv[3][1] == '\0')
+		while (argv[1][i] != '\0')
 		{
-			while (argv[1][i] != '\0')
-			{
-				if (argv[1][i] == argv[2][0])
-					argv[1][i] = argv[3][0];
-				write(1, &argv[1][i], 1);
-				i++;
-			}
+			if (argv[1][i] == argv[2][0])
+				argv[1][i] = argv[3][0];
+			write(1, &argv[1][i], 1);
+			i++;
 		}
 		write(1, "\n", 1);
 	}
