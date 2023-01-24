691755e127f440b6d6dc0cdfc5bda7fe409771b9
Amandine Demuynck <amdemuyn@c2r1s1.42madrid.com>
2023-01-24 20:29:00 +0100

    ft_strdup ok

---
+++ ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 19:58:22 by amdemuyn          #+#    #+#             */
+/*   Updated: 2023/01/24 20:18:10 by amdemuyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	char	*dest;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	dest = malloc(sizeof(char) * (i + 1));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+/*int	main(void)
+{
+printf("%s", ft_strdup("Ceci est un test"));
+}*/
