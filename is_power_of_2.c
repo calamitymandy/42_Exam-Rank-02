6f0547fa42c1f2ef0306b65b5686006a0b61ad8e
Amandine Demuynck <amdemuyn@c3r3s6.42madrid.com>
2023-03-22 15:22:50 +0100

    okay

---
+++ is_power_of_2.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_power_of_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 15:53:21 by amdemuyn          #+#    #+#             */
+/*   Updated: 2023/03/13 16:21:28 by amdemuyn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	is_power_of_2(unsigned int n)
+{
+	if (n == 0)
+		return (0);
+	while (n > 1)
+	{
+		if (n % 2 == 0)
+			n = n / 2;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/*int	main(void)
+{
+	unsigned int	n = 128;
+
+	printf("%d", is_power_of_2(n));
+	return (0);
+}*/
