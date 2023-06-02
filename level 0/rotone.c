12d045b236bc672f9057e6e46cf61a84f318127e
Amandine Demuynck <amdemuyn@c1r4s5.42madrid.com>
2023-06-02 18:31:58 +0200

    modifs rotone

--- level 0/rotone.c
+++ level 0/rotone.c
@@ -1,34 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rotone.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amdemuyn <amdemuyn@student.42madrid.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/12 17:49:09 by amdemuyn          #+#    #+#             */
-/*   Updated: 2023/01/12 18:08:38 by amdemuyn         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
+#include <stdio.h>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	if (argc == 2)
-	{
-		while (argv[1][i] != '\0')
-		{
-			if ((argv[1][i] >= 'a' && argv[1][i] <= 'y') || 
-					(argv[1][i] >= 'A' && argv[1][i] <= 'Y'))
-				argv[1][i] = argv[1][i] + 1;
-			else if (argv[1][i] == 'z' || argv[1][i] == 'Z')
-				argv[1][i] = argv[1][i] - 25;
-			write(1, &argv[1][i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+    i = 0;
+    if (argc == 2)
+    {
+        while (argv[1][i] != '\0')
+        {
+            if ((argv[1][i] >= 'a' && argv[1][i] <= 'y') || (argv[1][i] >= 'A' && argv[1][i] <= 'Y'))
+                argv[1][i] += 1;
+            else if (argv[1][i] == 'z' || argv[1][i] == 'Z')
+                argv[1][i] -= 25;
+            write(1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
 }
