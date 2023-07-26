3d859cb69746eb0da095f723aaa4a6447ff86169
Amandine Demuynck <amdemuyn@c1r6s4.42madrid.com>
2023-07-26 19:34:39 +0200

    ok

--- level 2/epur_str.c
+++ level 2/epur_str.c
@@ -1,33 +1,37 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * First find the end without spaces.
+ * Then find the start of the forst word.
+ * finally do the loop to write the string, whith inside another loop 
+ * to go on when there is spaces, until i + 1 is different of ' ' && '\t'
+ * BE CAREFUL THIS TIME IT'S && and not ||
+ */
 int main(int argc, char **argv)
 {
+    int i;
+    int end;
+
+    i = 0;
     if (argc == 2)
     {
-        int i;
-        int end;
-
-        i = 0;
-        end = 0;
-
         while (argv[1][i])
-            i++;
-        end = i - 1;
-
-        while (argv[1][end] == ' ' || argv[1][end] == '\t')
-            end--;
-        i = 0;
-        
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-            
-        while (i <= end)
-        {
-            if ((argv[1][i] == ' ' || argv[1][i] == '\t'))
-                write (1, " ", 1);
-            while (argv[1][i] == ' ' || argv[1][i] == '\t')
                 i++;
+        while (argv[1][i -1] == ' ' || argv[1][i -1] == '\t')
+            i--;
+        end = i -1;
+        i = 0;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+        i++;
+        while (argv[1][i] && i <= end)
+        {
+            while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            {
+                if (argv[1][i + 1] != ' ' && argv[1][i + 1] != '\t')
+                    write (1, " ", 1);
+                i++;
+            }
             write (1, &argv[1][i], 1);
             i++;
         }
