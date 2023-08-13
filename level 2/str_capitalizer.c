dc95ecbdb7cd55f68e72bcaba99c9eead9c54b5b
calamitymandy <amandined.plateforme@gmail.com>
2023-08-13 15:34:06 +0200

    not so bad

--- level 2/str_capitalizer.c
+++ level 2/str_capitalizer.c
@@ -2,43 +2,34 @@
 
 int main(int argc, char **argv)
 {
+    int i;
+    int j;
+
+    i = 1;
+    j = 0;
+
     if (argc >= 2)
     {
-        int i;
-        int j;
-        
-        i = 1;
-        j = 0;
         while (argv[i])
         {
-            while (argv[i][j])
+            while(argv[i][j])
             {
-                while (argv[i][j] && (argv[i][j] == ' ' || argv[i][j] == '\t'))
-                {
-                    write (1, &argv[i][j], 1);
-                    j++;
-                }
-                if (argv[i][j])
-                {
-                    if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-                        argv[i][j] -= 32;
-                    write (1, &argv[i][j], 1);
-                    j++;
-                }
-                while (argv[i][j] && argv[i][j] != ' ' && argv[i][j] != '\t')
-                {
-                    if (argv[i][j] >= 'A' && argv[i][j] <= 'Z')
-                        argv[i][j] += 32;
-                    write (1, &argv[i][j], 1);
-                    j++;
-                }
+                if (argv[i][j] >= 'A' && argv[i][j] <= 'Z' 
+                && argv[i][j - 1] != ' ' && argv[i][j - 1] != '\t' 
+                && j != 0)
+                    argv[i][j] += 32;
+                else if ((argv[i][j] >= 'a' && argv[i][j] <= 'z') 
+                && (argv[i][j - 1] == ' ' || argv[i][j - 1] == '\t' || j == 0))
+                    argv[i][j] -= 32;
+                write (1, &argv[i][j], 1);
+                j++;
             }
             j = 0;
-            write (1, "\n", 1);
+            write(1, "\n", 1);
             i++;
         }
     }
-    else
-    write (1, "\n", 1);
+    else 
+        write(1, "\n", 1);
     return (0);
 }
