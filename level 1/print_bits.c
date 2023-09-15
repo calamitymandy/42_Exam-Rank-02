c4d187cba90f3e361383d85eed983751b280f555
Amandine Demuynck <amdemuyn@c1r15s4.42madrid.com>
2023-09-15 18:49:19 +0200

    print_bits

--- level 1/print_bits.c
+++ level 1/print_bits.c
@@ -1,13 +1,24 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void	print_bits(unsigned char octet)
 {
-    int i = 8;
-    unsigned char bit = 0;
+	int i = 8;
+	unsigned char bit = 0;
 
-    while (i--)
-    {
-        bit = (octet >> i & 1) + '0';
-        write (1, &bit, 1);
-    }
+	while (i--)
+	{
+		if (octet & 1 << i)
+			write (1, "1", 1);
+		else
+			write (1, "0", 1);
+	}
 }
+
+/*
+int main(void)
+{
+	unsigned char octet = 3;
+	print_bits(octet);
+}
+*/
