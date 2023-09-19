fb1485b01decd04e96f563f0d1f0e5fc1b9501be
Amandine Demuynck <amdemuyn@c1r3s4.42madrid.com>
2023-09-19 19:45:51 +0200

    fuck

--- level 2/hidenp.c
+++ level 2/hidenp.c
@@ -1,39 +1,24 @@
 #include <unistd.h>
-#include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int i = 0;
-    int j = 0;
-    int count = 0;
-    int len = 0;
+	int i = 0;
+	int j = 0;
+	int len = 0;
 
-    if (argc == 3)
-    {
-        while (argv[1][i])
-            i++;
-        len = i;
-        i = 0;
-        while (argv[1][i] && count < len)
-        {
-            while (argv[2][j])
-            {
-                if (argv[1][i] == argv[2][j])
-                {
-                    count++;
-                    i++;
-                    j++;
-                }
-                else
-                    j++;
-            }
-            i++;
-        }
-        if (count == len)
-            write (1, "1", 1);
-        else
-            write (1, "0", 1);
-    }
-    write (1, "\n", 1);
-    return (0);
+	if (argc == 3)
+	{
+		while (argv[2][j] && argv[1][i])
+		{
+			if (argv[1][i] == argv[2][j])
+				i++;
+			j++;
+		}
+		if (!argv[1][i])
+			write (1, "1", 1);
+		else
+			write (1, "0", 2);
+	}
+	write (1, "\n", 1);
+	return (0);
 }
